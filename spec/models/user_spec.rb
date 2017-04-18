eca956ff4428f0d5132008c6a7c95f0c67973bd8
Fabiano F. Scalco <fabiano.scalco@gmail.com>
2017-04-18 20:44:08 +1000

    Added initial tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryGirl.build(:user)
+  end
+
+  context "validations" do
+    describe "attributes" do
+      it "must have an email" do
+        expect(@user).to respond_to(:email)
+      end
+
+      it "must have a name" do
+        expect(@user).to respond_to(:name)
+      end
+
+      it "must have a password" do
+        expect(@user).to respond_to(:password)
+      end
+
+      it "must have a password_confirmation" do
+        expect(@user).to respond_to(:password_confirmation)
+      end
+    end
+
+    it "must be valid" do
+      expect(@user).to be_valid
+    end
+  end
 end
