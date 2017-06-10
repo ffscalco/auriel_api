3c70981d064f96075b8ab22f0c54a748fcaa4f21
Fabiano F. Scalco <fabiano.scalco@gmail.com>
2017-06-10 13:13:33 +1000

    changing double quotes to simple quotes.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context "validations" do
-    describe "email" do
-      it "must be present" do
+  context 'validations' do
+    describe 'email' do
+      it 'must be present' do
         is_expected.to validate_presence_of(:email)
       end
 
-      it "must be uniq" do
+      it 'must be uniq' do
         is_expected.to validate_uniqueness_of(:email).case_insensitive
       end
 
-      it "must be in the correct format" do
-        is_expected.to allow_value("test@email.com").for(:email)
+      it 'must be in the correct format' do
+        is_expected.to allow_value('test@email.com').for(:email)
       end
     end
 
-    it "password must be confirmed" do
+    it 'password must be confirmed' do
       is_expected.to validate_confirmation_of(:password)
     end
   end
